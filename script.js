06386d2362262a028c5009a452baf148765cc415
Sanyam jain <Sanyam0029@gmail.com>
2022-05-23 14:38:14 +0530

    Delete item

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ document.addEventListener('keypress', (e)=>{
     add.click();
     }
 });
-console.log('Enter key pressed');
-    add.click();
+
 add.addEventListener('click', function(){
    var new_element = text.value;
    var item = document.createElement('li');
+   var delete_button = document.createElement('button');
+   delete_button.addEventListener('click', function(){
+       item.remove();
+       delete_button.remove();
+   })
+   delete_button.innerHTML = 'Remove';
    item.innerHTML = new_element;
+   item.appendChild(delete_button);
    list.appendChild(item);
    text.value = '';
 })
